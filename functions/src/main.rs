983ee7791d0fbb4e1637b3c651f479adab4f3b9c
Lance Smith <lance@huckberry.com>
2020-04-27 00:20:30 -0700

    Functions and loops

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,17 @@ fn main() {
     println!("Hello, world!");
 
     another_function(101, 9);
+
+    println!(
+        "Implicit return from other function is {}.",
+        other_function()
+    );
 }
 
 fn another_function(num1: i32, num2: i32) {
     println!("Another function received {} and {}.", num1, num2);
+}
+
+fn other_function() -> i32 {
+    1101
 }
