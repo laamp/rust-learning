ad1c623ed7e94a3b16b5e4164240b6f89a67d7be
Lance Smith <lance@huckberry.com>
2020-05-03 15:53:37 -0700

    Finish structs; start enums

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,36 +1,25 @@
 fn main() {
-    // let width1 = 10;
-    // let height1 = 10;
-
-    // let rect = (2, 75);
-
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         color: "green".to_string(),
-        width: 7,
-        height: 7,
+        width: 30,
+        height: 50,
     };
 
-    // println!("The area of the rectangle is {}", area(width1, height1));
+    let rect2 = Rectangle {
+        color: "red".to_string(),
+        width: 10,
+        height: 40,
+    };
 
-    // println!("The area of the rectangle is {}", area(rect));
+    let rect3 = Rectangle {
+        color: "blue".to_string(),
+        width: 60,
+        height: 45,
+    };
 
-    println!("The area of the rectangle is {}", rect.area());
-    println!("The color of the rectangle is {}", rect.get_color());
-
-    println!("{:#?}", rect);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
-
-// fn area(w: u32, h: u32) -> u32 {
-//     w * h
-// }
-
-// fn area(dimensions: (u32, u32)) -> u32 {
-//     dimensions.0 * dimensions.1
-// }
-
-// fn area(rectangle: &Rectangle) -> u32 {
-//     rectangle.height * rectangle.width
-// }
 
 #[derive(Debug)]
 struct Rectangle {
@@ -40,6 +29,10 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        (self.width > other_rect.width) && (self.height > other_rect.height)
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
     }
